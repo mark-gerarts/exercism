6cf96395cd97799b584d6e8815b87b06026a1c04
Mark Gerarts <mark.gerarts@gmail.com>
2019-05-30 17:09:14 +0200

    Improve space age

--- haskell/space-age/src/SpaceAge.hs
+++ haskell/space-age/src/SpaceAge.hs
@@ -9,18 +9,24 @@ data Planet = Mercury
             | Uranus
             | Neptune
 
-normalizeToEarthYear :: Float -> Float -> Float
-normalizeToEarthYear ratio = (/ (ratio * 31557600))
+type Seconds = Float
+
+earthYear :: Seconds
+earthYear = 60 * 60 * 24 * 365.25
 
 ageOn :: Planet -> Float -> Float
-ageOn planet = normalizeToEarthYear ratio
-  where
-    ratio = case planet of
-      Earth -> 1.0
-      Mercury -> 0.2408467
-      Venus -> 0.61519726
-      Mars -> 1.8808158
-      Jupiter -> 11.862615
-      Saturn -> 29.447498
-      Uranus -> 84.016846
-      Neptune -> 164.79132
+ageOn = normalizeToEarthYear . orbitalPeriod
+
+normalizeToEarthYear :: Float -> Float -> Float
+normalizeToEarthYear ratio = (/ (ratio * earthYear))
+
+orbitalPeriod :: Planet -> Float
+orbitalPeriod planet = case planet of
+     Earth -> 1.0
+     Mercury -> 0.2408467
+     Venus -> 0.61519726
+     Mars -> 1.8808158
+     Jupiter -> 11.862615
+     Saturn -> 29.447498
+     Uranus -> 84.016846
+     Neptune -> 164.79132
